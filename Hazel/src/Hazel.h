33044bc20a9fc2c56c07e3d62d98c7929da40c13
miyanyan <1138989048@qq.com>
2022-08-17 16:59:19 +0800

    add key and mouse codes

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -4,5 +4,8 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 #include "Hazel/Application.h"
 #include "Hazel/Log.h"
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
 
 #include "Hazel/EntryPoint.h"
