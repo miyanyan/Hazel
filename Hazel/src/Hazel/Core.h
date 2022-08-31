b46d67ffae1f4c93207d4698a1c79ec8a758c0f1
miyanyan <1138989048@qq.com>
2022-08-31 17:55:32 +0800

    use static library instead

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#if HZ_DYNAMIC_LINK
 #ifdef HZ_BUILD_DLL
-	#define HAZEL_API __declspec(dllexport)
+#define HAZEL_API __declspec(dllexport)
 #else
-	#define HAZEL_API __declspec(dllimport)
-#endif // HZ_BUILD_DLL
+#define HAZEL_API __declspec(dllimport)
+#endif
+#else
+#define HAZEL_API
+#endif
 
 #define BIT(x) (1 << x)
