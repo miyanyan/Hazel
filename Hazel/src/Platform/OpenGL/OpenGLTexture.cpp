cc28d5021a554c42f0ba4a3acc425459c08b1b8e
miyanyan <1138989048@qq.com>
2022-09-05 14:04:29 +0800

    support RGB and RGBA format texture

--- Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -17,14 +17,31 @@ namespace Hazel {
 
 		int channels;
 		auto data = stbi_load(path, &m_width, &m_height, &channels, 0);
+
 		if (!data) {
 			HZ_CORE_ERROR("failed to load image({})!", path);
 		}
 		else {
-			glTextureStorage2D(m_textureId, 1, GL_RGB8, m_width, m_height);
-			glTextureParameteri(m_textureId, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-			glTextureParameteri(m_textureId, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTextureSubImage2D(m_textureId, 0, 0, 0, m_width, m_height, GL_RGB, GL_UNSIGNED_BYTE, data);
+			// format
+			GLenum internalFormat = 0, dataFormat = 0;
+			if (channels == 4) {
+				internalFormat = GL_RGBA8;
+				dataFormat = GL_RGBA;
+			}
+			else if (channels == 3) {
+				internalFormat = GL_RGB8;
+				dataFormat = GL_RGB;
+			}
+
+			if (internalFormat != 0 && dataFormat != 0) {
+				glTextureStorage2D(m_textureId, 1, internalFormat, m_width, m_height);
+				glTextureParameteri(m_textureId, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTextureParameteri(m_textureId, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				glTextureSubImage2D(m_textureId, 0, 0, 0, m_width, m_height, dataFormat, GL_UNSIGNED_BYTE, data);
+			}
+			else {
+				HZ_CORE_ERROR("not supported format, image({})!", path);
+			}
 		}
 
 		stbi_image_free(data);
