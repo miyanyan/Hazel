4319af850175d92b936a5bb4fda3bec7a08b7411
miyanyan <1138989048@qq.com>
2022-09-21 11:41:12 +0800

    update drawQuad function

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -311,85 +311,32 @@ namespace Hazel {
 		s_data.stats.quadCount++;
 	}
 
-	void Renderer2D::drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color, float rotation)
+	void Renderer2D::drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
-		drawRotatedQuad({ position.x, position.y, 0.0f }, size, color, rotation);
+		drawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, color);
 	}
 
-	void Renderer2D::drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color, float rotation)
+	void Renderer2D::drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
-		if (s_data.quadIndexCount >= Renderer2DStorage::maxIndices)
-			flushAndReset();
-
-		constexpr float textureIndex = 0.0f; // White Texture
-		constexpr float tilingFactor = 1.0f;
-		constexpr size_t quadVertexCount = 4;
-		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
-
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		for (size_t i = 0; i < quadVertexCount; ++i) {
-			s_data.quadVertexBufferPtr->position = transform * s_data.quadVertexPositions[i];
-			s_data.quadVertexBufferPtr->color = color;
-			s_data.quadVertexBufferPtr->texCoord = textureCoords[i];
-			s_data.quadVertexBufferPtr->texIndex = textureIndex;
-			s_data.quadVertexBufferPtr->tilingFactor = tilingFactor;
-			s_data.quadVertexBufferPtr++;
-		}
-
-		s_data.quadIndexCount += 6;
-
-		s_data.stats.quadCount++;
+		drawQuad(transform, color);
 	}
 
-	void Renderer2D::drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const std::shared_ptr<Texture2D>& texture, float rotation, float tilingFactor, const glm::vec4& tintcolor)
+	void Renderer2D::drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const std::shared_ptr<Texture2D>& texture, float tilingFactor, const glm::vec4& tintcolor)
 	{
-		drawRotatedQuad({ position.x, position.y, 0.0f }, size, texture, rotation, tilingFactor, tintcolor);
+		drawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, texture, tilingFactor, tintcolor);
 	}
 
-	void Renderer2D::drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const std::shared_ptr<Texture2D>& texture, float rotation, float tilingFactor, const glm::vec4& tintcolor)
+	void Renderer2D::drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const std::shared_ptr<Texture2D>& texture, float tilingFactor, const glm::vec4& tintcolor)
 	{
-		if (s_data.quadIndexCount >= Renderer2DStorage::maxIndices)
-			flushAndReset();
-
-		constexpr size_t quadVertexCount = 4;
-		constexpr glm::vec2 textureCoords[] = { { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 1.0f }, { 0.0f, 1.0f } };
-
-		float textureIndex = 0.0f;
-		for (uint32_t i = 1; i < s_data.textureSlotIndex; i++) {
-			if (*s_data.textureSlots[i].get() == *texture.get()) {
-				textureIndex = (float)i;
-				break;
-			}
-		}
-
-		if (textureIndex == 0.0f) {
-			if (s_data.textureSlotIndex >= Renderer2DStorage::maxtextureSlots) {
-				flushAndReset();
-			}
-			textureIndex = (float)s_data.textureSlotIndex;
-			s_data.textureSlots[s_data.textureSlotIndex] = texture;
-			s_data.textureSlotIndex++;
-		}
-
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		for (size_t i = 0; i < quadVertexCount; ++i) {
-			s_data.quadVertexBufferPtr->position = transform * s_data.quadVertexPositions[i];
-			s_data.quadVertexBufferPtr->color = tintcolor;
-			s_data.quadVertexBufferPtr->texCoord = textureCoords[i];
-			s_data.quadVertexBufferPtr->texIndex = textureIndex;
-			s_data.quadVertexBufferPtr->tilingFactor = tilingFactor;
-			s_data.quadVertexBufferPtr++;
-		}
-
-		s_data.quadIndexCount += 6;
-
-		s_data.stats.quadCount++;
+		drawQuad(transform, texture, tilingFactor, tintcolor);
 	}
 
 	void Renderer2D::resetStats()
--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -25,11 +25,11 @@ namespace Hazel {
 		static void drawQuad(const glm::mat4& transform, const glm::vec4& color);
 		static void drawQuad(const glm::mat4& transform, const std::shared_ptr<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 
-		static void drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color, float rotation);
-		static void drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color, float rotation);
+		static void drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+		static void drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
 
-		static void drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const std::shared_ptr<Texture2D>& texture, float rotation, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-		static void drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const std::shared_ptr<Texture2D>& texture, float rotation, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+		static void drawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const std::shared_ptr<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+		static void drawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const std::shared_ptr<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 	
 		// Stats
 		struct Statistics
